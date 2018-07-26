55aec7d183c24e3e0fed90f184f62cc7feff5596
golfgabkung <golfgabkung@gmail.com>
2018-07-26 16:51:25 +0700

    [Created]: สร้างฟังก์ชัน PostAddEmpolyeeHandler with test

--- src/employee/api/api.go
+++ src/employee/api/api.go
@@ -1,20 +1,39 @@
 package api
 
 import (
-    "net/http"
-    "employee/service"
+	"employee/service"
+
+	"employee/model"
+	"encoding/json"
+	"net/http"
 )
 
 type API struct {
-    Service service.IService
+	Service service.IService
 }
 
-func (api *API) DeleteEmployeeHandler(writer http.ResponseWriter, request *http.Request)  {
-    id := request.URL.Query().Get("id")
-    status := api.Service.DeleteEmployee(id)
-    if status{
-        writer.WriteHeader(http.StatusOK)
-        return
-    }
-    http.Error(writer,"Delete Error",http.StatusInternalServerError)
+func (api *API) DeleteEmployeeHandler(writer http.ResponseWriter, request *http.Request) {
+	id := request.URL.Query().Get("id")
+	status := api.Service.DeleteEmployee(id)
+	if status {
+		writer.WriteHeader(http.StatusOK)
+		return
+	}
+	http.Error(writer, "Delete Error", http.StatusInternalServerError)
+}
+
+func (api *API) PostAddEmpolyeeHandler(write http.ResponseWriter, request *http.Request) {
+	employee := model.EmployeeRequest{}
+	err := json.NewDecoder(request.Body).Decode(&employee)
+	if err != nil {
+		http.Error(write, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	newEmployeeResponse, err := api.Service.AddEmployee(employee)
+	if err != nil {
+		http.Error(write, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	newEmployeeResponseJson, _ := json.Marshal(newEmployeeResponse)
+	write.Write(newEmployeeResponseJson)
 }
